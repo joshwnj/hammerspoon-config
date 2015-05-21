8a300340beadc099e6df12237b8523af4e2bf733
Josh Johnston <josh@x-team.com>
2015-05-21 22:51:49 +1000

    global mute with visual indicator

--- init.lua
+++ init.lua
@@ -31,3 +31,71 @@ end
 hotkey.bind(cac, 'M', toggleSkypeMute)
 
 ----
+
+local muteCircle = nil
+function removeCircle(circle)
+  if circle then
+    circle:delete()
+  end
+end
+
+function createMuteCircle()
+  muteCircle = hs.drawing.circle(hs.geometry.rect(20, 40, 80, 80))
+  muteCircle:setFillColor({["red"]=0, ["green"]=0, ["blue"]=0, ["alpha"]=0.5})
+  muteCircle:setFill(true)
+  muteCircle:setStroke(true)
+end
+
+function drawMuteCircle()
+  removeCircle(muteCircle)
+  createMuteCircle()
+  muteCircle:show()
+end
+
+----
+-- source: https://github.com/squaresurf/hammerspoon-castle/blob/master/home/.hammerspoon/init.lua#L70-L95
+
+function micIsMuted()
+    status, volume = hs.applescript.applescript('get volume settings')
+    if status then
+        invl = volume:match("invl':(%d+)")
+        if invl == '0' then
+            return true
+        else
+            return false
+        end
+    end
+end
+
+function displayIfMicIsMuted()
+    muted = micIsMuted()
+    if muted == nil then
+        hs.alert.show("Not sure if the Mic is muted.")
+    elseif muted then
+      drawMuteCircle()
+    else
+      removeCircle(muteCircle)
+    end
+end
+
+-- Toggle mic mute
+function muteMic()
+  hs.applescript.applescript('set volume input volume 0')
+end
+
+function unmuteMic()
+  hs.applescript.applescript('set volume input volume 75')
+end
+
+function toggleMicMute()
+    muted = micIsMuted()
+    if muted then
+      unmuteMic()
+    else
+      muteMic()
+    end
+    displayIfMicIsMuted()
+end
+
+-- enable the mic by default
+unmuteMic()
